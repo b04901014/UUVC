66c0ec48e79da28a98644573a3994a8f15c23996
Li-Wei Chen <liweiche@altali.speech.cs.cmu.edu>
2022-11-01 15:52:24 -0400

    adjust parameter

--- train.py
+++ train.py
@@ -25,7 +25,6 @@ parser.add_argument('--batch_size', type=float, default=200)
 parser.add_argument('--train_bucket_size', type=int, default=32)
 parser.add_argument('--val_batch_size', type=float, default=50)
 parser.add_argument('--val_bucket_size', type=int, default=10)
-parser.add_argument('--valid_split', type=float, default=0.95)
 parser.add_argument('--training_step', type=int, default=300000)
 parser.add_argument('--freeze_module_step', type=int, default=None)
 parser.add_argument('--optim_flat_percent', type=float, default=0.0)
@@ -48,11 +47,6 @@ parser.add_argument('--voiced_hidden_size', type=int, default=128)
 parser.add_argument('--dp_hidden_size', type=int, default=128)
 parser.add_argument('--E_hidden_size', type=int, default=256)
 parser.add_argument('--feature_size', type=int, default=256)
-parser.add_argument('--no_texture', action='store_true')
-
-#Ablation
-parser.add_argument('--ablated_mixed', action='store_true')
-parser.add_argument('--continuous_S3R', action='store_true')
 
 #Dropout
 parser.add_argument('--dropout', type=float, default=0.1)
@@ -63,7 +57,7 @@ parser.add_argument('--dp_dropout', type=float, default=0.3)
 parser.add_argument('--E_dropout', type=float, default=0.3)
 
 #Trainer
-parser.add_argument('--check_val_every_n_epoch', type=int, default=5)
+parser.add_argument('--check_val_every_n_epoch', type=int, default=1)
 parser.add_argument('--precision', type=str, choices=['16', '32', "bf16"], default=32)
 parser.add_argument('--nworkers', type=int, default=16)
 parser.add_argument('--distributed', action='store_true')
@@ -102,7 +96,6 @@ if args.accelerator == 'ddp':
 checkpoint_callback = ModelCheckpoint(
     dirpath=args.saving_path,
     filename=(fname_prefix+'{epoch}-{step}'),
-#    every_n_train_steps=(None if args.val_check_interval == 1.0 else args.val_check_interval),
     every_n_epochs=(None if args.check_val_every_n_epoch == 1 else args.check_val_every_n_epoch),
     verbose=True,
     save_last=True
@@ -119,7 +112,6 @@ wrapper = Trainer(
     amp_backend='native',
     callbacks=[checkpoint_callback],
     resume_from_checkpoint=args.resume_checkpoint,
-#    val_check_interval=args.val_check_interval,
     num_sanity_val_steps=0,
     max_steps=args.training_step,
     gpus=(-1 if args.distributed else 1),
